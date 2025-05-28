f42d31ae299fded18f19647d1680e47b4cbef02f
JeremyCCHsu <jeremycchsu@gmail.com>
2025-05-28 15:57:27 +0000

    init
    ---------
    
    Co-authored-by: fatchord <fatchord@tutanota.com>
    Co-authored-by: manmay nakhashi <manmay.nakhashi@gmail.com>
    Co-authored-by: John Meade <john.meade@hbar.ca>
    Co-authored-by: Zohaib Ahmed<zohaib92@gmail.com>

---
+++ example_tts.py
@@ -0,0 +1,9 @@
+import torchaudio as ta
+from chatterbox.tts import ChatterboxTTS
+
+model = ChatterboxTTS.from_pretrained(device="cuda")
+
+text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
+wav = model.generate(text)
+ta.save("test-1.wav", wav, model.sr)
+
